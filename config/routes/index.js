b6e81cab7aea2ba369dc8d4eb32d46a5da697f56
Adesuwa T. Dawodu <littlezigy@gmail.com>
2020-06-20 17:14:42 +0100

    Added routes to root level without login

--- config/routes/index.js
+++ config/routes/index.js
@@ -3,10 +3,21 @@ const reports = require('../../components/health/report.ctrl');
 const user = require('../../components/auth/auth.ctrl');
 const notification = require('../../components/notifications/notifications.ctrl');
 
+let links = {
+    create_admin_account: '/admin/create-account',
+    signup: '/signup',
+    login: '/login',
+    create_reminder: '/reminders/new',
+    fill_health_form: '/forms/health',
+    fill_quarantine_form: '/forms/quarantine',
+}
+
 router.get('/', function(req, res) {
     if(!req.session.user_id) {
         res.link('signup', '/signup');
         res.link('login', '/login');
+        res.link('health_form', links.fill_health_form);
+        res.link('quarantine_form', links.fill_quarantine_form);
     } else {
         res.link('fill_form_quarantine', '/forms/quarantine');
     }
